e40d4d5f931a4e0d62fb5564ee52ab8bd617a51f
Naira98 <nairamm99@gmail.com>
2024-07-19 19:26:03 +0300

    cleanup

--- src/features/cabins/CreateCabinForm-v1.jsx
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -1,6 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
